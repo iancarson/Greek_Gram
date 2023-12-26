041439a1a6b4f17b913b1790d21d1c6c6ac1f96b
Ian Carson <38280903+iancarson@users.noreply.github.com>
2023-12-26 16:22:24 -0500

    Update index.js
    Refactor the comments to enhance code comprehension

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ import App from './App';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up 
+// the environment is set up
 registerRootComponent(App);
