fc7499fd96654a9122e7c621a64bee6544728e67
Ian Carson <38280903+iancarson@users.noreply.github.com>
2023-12-27 19:24:27 -0500

    Update index.js
    Refactor the comments to enhance comprehension

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ import { registerRootComponent } from 'expo';
 import App from './App';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a nat
+// It also ensures that whether you load the app in Expo Go or in a na
 registerRootComponent(App);
