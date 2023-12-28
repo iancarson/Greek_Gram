4a8e4e6c0e2ecc5672a6441c691f33062bee3ba7
Ian Carson <38280903+iancarson@users.noreply.github.com>
2023-12-27 19:28:17 -0500

    Update index.js
    Refactor the comments to enhance comprehension

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ import { registerRootComponent } from 'expo';
 import App from './App';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app 
+// It also ensures that whether you load the app
 registerRootComponent(App);
