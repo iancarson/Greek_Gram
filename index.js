0ac2bab3ee361524605f9207263d87e3aca156b7
Ian Carson <38280903+iancarson@users.noreply.github.com>
2023-12-31 12:18:03 -0500

    Update index.js
    Refactor the comments to enhance comprehension

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ import { registerRootComponent } from 'expo';
 
 import App from './App';
 
-// registerRootComponent calls AppRegistry.r
+// registerRootComponent calls AppRegistry.
 registerRootComponent(App);
